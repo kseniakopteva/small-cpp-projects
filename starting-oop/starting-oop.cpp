f6d1af76ddcef5eb8f29e5a93c4e1b584a692908
Ksenia <ksenija.kopteva@gmail.com>
2020-07-29 21:01:44 +0300

    added project with oop stuff

---
+++ starting-oop/starting-oop.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+
+private:
+	int age;
+	string name;
+
+public:	
+
+	Person() { //конструктор
+		name = "Default name"; 
+		age = 0;
+	}
+	Person(string name, int age) { //перегрузка констркторов
+		this->age = age;
+		this->name = name;
+	}
+
+	//конструктор копирования
+	//(нужен чтобы корректно удалить динамическую память)
+	Person(const Person &other) {
+		//...
+	}
+	
+	string getName() {
+		return name;
+	}
+	int getAge() {
+		return age;
+	}
+
+	void setAge(int age) {
+		this->age = age;
+	}
+
+	void output() {
+		cout << "name: " << name << endl
+			<< "age: " << age << endl;
+	}
+};
+
+int main() {
+
+	Person a("Masha", 15);
+	Person b;
+
+	a.output();
+	b.output();
+
+	
+
+}
